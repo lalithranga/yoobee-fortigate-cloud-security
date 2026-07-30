593b598dc17f3415ecc411db1fedef8ae46f5e2a
lalithranga <wkdlkumara@gmail.com>
2026-07-30 15:12:27 +1200

    docs: validated all code

---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "aws_region" {
+  type        = string
+  description = "The target AWS region for deployment"
+  default     = "ap-southeast-2" # Sydney region as required by assignment
+}
